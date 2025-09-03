9b3f9a9a789294252e21e26432e90c834b7f1df0
johnbullkitchner1 <kitchnerbull@gmail.com>
2025-09-03 12:32:52 +0100

    hidden new relic code

--- newrelic.js
+++ newrelic.js
@@ -1,5 +1,5 @@
 exports.config = {
     app_name: ['MySampleApp'],
-    license_key: 'eu01xx79e52afb6cccaa3df724b7e192FFFFNRAL', // Replace with key from New Relic > Add data > Node.js
+    license_key: '', 
     logging: { level: 'debug' }
   };
